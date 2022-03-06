3dcebe81ba5412021a9adfc32e8aff8612f9e4c1
Chupriarti <wallx13@gmail.com>
2022-03-07 00:11:08 +0300

    use describe

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i);
-  const btn = screen.getByRole('button');
-  const input = screen.getByPlaceholderText(/input value/i);
-  expect(helloWorldElem).toBeInTheDocument();
-  expect(btn).toBeInTheDocument();
-  expect(input).toBeInTheDocument();
-  expect(input).toMatchSnapshot();
-});
+describe('TEST APP', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+})
