e77c41d433e0f2bacb3d306f492788746b44b31b
Chupriarti <wallx13@gmail.com>
2022-03-08 22:57:05 +0300

    add test click event

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('TEST APP', () => {
@@ -20,5 +20,15 @@ describe('TEST APP', () => {
     const helloWorldElem = await screen.findByText(/data/i)
     expect(helloWorldElem).toBeInTheDocument()
     expect(helloWorldElem).toHaveStyle({color: 'red'})
+  });
+
+  test('test cleck event', async () => {
+    render(<App />)
+    const btn = screen.getByTestId('toggle-btn')
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
   });
 })
