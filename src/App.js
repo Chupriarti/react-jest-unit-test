56d253f4864f9b0c061703d9a51d060dbfc3401f
Chupriarti <wallx13@gmail.com>
2022-03-08 22:50:54 +0300

    prepare layout for test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 function App() {
   const [data, setData] = React.useState(null)
+  const [toggle, setToggle] = React.useState(false)
+
+  const onClick = () => setToggle (prev => !prev)
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -11,9 +14,10 @@ function App() {
 
   return (
     <div >
+      {toggle === true  && <div>toggle</div> }
       {data && <div style={{color: 'red'}}>data</div>}
       <h1>Hello world</h1>
-      <button>click me</button>
+      <button onClick={onClick}>click me</button>
       <input type="text" placeholder='input value...' />
     </div>
   );
