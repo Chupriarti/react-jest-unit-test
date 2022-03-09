216b18327d6021429153d4030ba97b9f5d16178a
Chupriarti <wallx13@gmail.com>
2022-03-09 22:57:14 +0300

    add users state

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,6 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 
 const Users = () => {
+    const [users, setUsers] = React.useState([])
+
+    const loadUsers = async () => {
+        const resp = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setUsers(resp.data)
+    }
+
+    React.useState(() => {
+        loadUsers()
+    }, [])
+
     return (
         <div>
 
