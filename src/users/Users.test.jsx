c7150bc5c431877b938aeb0cd1c744ce2b5c0181
Chupriarti <wallx13@gmail.com>
2022-03-09 23:04:35 +0300

    add users test mock

--- src/users/Users.test.jsx
+++ src/users/Users.test.jsx
@@ -1,7 +1,29 @@
 import { render } from '@testing-library/react'
 import Users from './Users'
+import axios from 'axios'
+
+jest.mock('axios')
 
 describe('users test', () => {
+    let response
+    beforeEach (() => {
+        response = {
+            data: [
+                {
+                    "id": 1,
+                    "name": "Leanne Graham",
+                },
+                {
+                    "id": 2,
+                    "name": "Ervin Howell",
+                },
+                {
+                    "id": 3,
+                    "name": "Clementine Bauch",
+                }
+            ]
+        }
+    })
     test('renders', () => {
         render(<Users />)
     })
