5ff67f0a14478ee48f77097e6c950b3e701745f7
Chupriarti <wallx13@gmail.com>
2022-03-10 23:50:10 +0300

    add app routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
+import MainPage from './pages/MainPage'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Link to="/" data-testid="main-link">Main Page</Link>
+      <Link to="/about" data-testid="about-link">About Page</Link>
       <Routes>
-
+        <Route path="/" element= {<MainPage />} />
+        <Route path="/about" element= {<AboutPage />} />
       </Routes>
     </BrowserRouter>
   )
