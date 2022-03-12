b18d0fa2900564a0783a57e41a5bcecede4d73a1
Chupriarti <wallx13@gmail.com>
2022-03-12 23:50:46 +0300

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 import AppRouter from './router/AppRouter'
 
 const App = () => {
   return (
     <div>
-      <Link to="/" data-testid="main-link">Main Page</Link>
-      <Link to="/about" data-testid="about-link">About Page</Link>
-      <Link to="/users" data-testid="users-link">Users Page</Link>
+      <Navbar />
       <AppRouter />
     </div>
   )
