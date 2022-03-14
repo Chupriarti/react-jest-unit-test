21a0084ce8c49dbc6132c227d7d7a346049e3cc1
Chupriarti <wallx13@gmail.com>
2022-03-14 23:35:23 +0300

    use selector and dispatch

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from '../../store/reducers/counterReducer'
+import { getCounterValue } from '../../store/reducers/selectors/getCounterValue/getCounterValue'
 
 const Counter = () => {
     const dispatch = useDispatch()
-    const value = useSelector()
+    const value = useSelector(getCounterValue)
 
     const onIncrement = () => {
-
+        dispatch(increment())
     }
 
     const onDecrement = () => {
-        
+        dispatch(decrement())
     }
 
     return (
