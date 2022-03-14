c35188646396eb62c04b0554a9fcc1ff2ba376f1
Chupriarti <wallx13@gmail.com>
2022-03-14 23:50:58 +0300

    add filled state test

--- src/store/reducers/selectors/getCounterValue/getCounterValue.test.js
+++ src/store/reducers/selectors/getCounterValue/getCounterValue.test.js
@@ -4,4 +4,12 @@ describe('getCounterValue', () => {
     test('work with empty state', () => {
          expect(getCounterValue({})).toBe(0)
     })
+
+    test('work with filled state', () => {
+        expect(getCounterValue({
+            counter: {
+                value: 100
+            }
+        })).toBe(100)
+   })
 })
