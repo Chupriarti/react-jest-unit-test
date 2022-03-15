132c75ef9e19af03b5cd3b788ac9ba2487935460
Chupriarti <wallx13@gmail.com>
2022-03-15 23:41:38 +0300

    add decrement reducer test

--- src/store/reducers/counterReducer.test.js
+++ src/store/reducers/counterReducer.test.js
@@ -4,4 +4,8 @@ describe('getCounterValue', () => {
   test('increment', () => {
     expect(counterReducer({value: 0}, increment())).toEqual({value: 1})
   })
+
+  test('decrement', () => {
+    expect(counterReducer({value: 0}, decrement())).toEqual({value: -1})
+  })
 }) 
