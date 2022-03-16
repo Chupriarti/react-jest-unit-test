01863ec1a54827552a747e924de213e7add8351a
Chupriarti <wallx13@gmail.com>
2022-03-16 23:52:38 +0300

    fix test by adding store

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -1,11 +1,19 @@
 import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
+import { Provider } from "react-redux"
 import Counter from "./Counter"
+import { createReduxStore } from '../../store/store';
 
 
 describe('Counter test', () => {
     test('Basic test', async () => {
-        const {getByTestId} = render(<Counter />)
+        const {getByTestId} = render(
+            <Provider 
+                store={createReduxStore()}
+            >
+                <Counter />
+            </Provider>
+        )
 
         const incrementBtn = getByTestId('increment-btn')
         expect(getByTestId('value-title')).toHaveTextContent('0')
