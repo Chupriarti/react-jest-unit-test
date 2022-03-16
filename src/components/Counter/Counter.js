b3d6306dc0f4ac21502110e1d91090c5ae0bf5c3
Chupriarti <wallx13@gmail.com>
2022-03-16 23:41:47 +0300

    prepare Counter for testing

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -17,9 +17,9 @@ const Counter = () => {
 
     return (
         <div>
-            <h1>Value = {value}</h1>
-            <button onClick={onIncrement}>Increment</button>
-            <button onClick={onDecrement}>Decrement</button>
+            <h1 data-testid="value-title">Value = {value}</h1>
+            <button data-testid="increment-btn" onClick={onIncrement}>Increment</button>
+            <button data-testid="decrement-btn" onClick={onDecrement}>Decrement</button>
         </div>
     )
 }
